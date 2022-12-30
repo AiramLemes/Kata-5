9ee71848d28859313d35d07920c90a8f11b79b85
AiramLemes <90828450+AiramLemes@users.noreply.github.com>
2022-12-30 16:49:57 +0000

    Versión 1 de la kata 5

---
+++ src/kata5/SelectApp.java
@@ -0,0 +1,54 @@
+
+package kata5;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class SelectApp {
+    
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:flights.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+         System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    
+    // Se seleccionan todos los registros de la tabla PEOPLE
+    public void selectAll(){
+        String sql = "SELECT * FROM FLIGHTS";
+        
+        try (Connection conn = this.connect();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql)){
+            
+            // Bucle sobre el conjunto de registros.
+            
+            while (rs.next()) {
+                System.out.println(
+                    rs.getString("DAY_OF_WEEK") + "\t" +
+                    rs.getString("DEP_TIME") + "\t" +
+                    rs.getString("DEP_DELAY") + "\t" +
+                    rs.getString("ARR_TIME") + "\t" +
+                    rs.getString("ARR_DELAY") + "\t" +
+                    rs.getString("CANCELLED") + "\t" +
+                    rs.getString("AIR_TIME") + "\t" +
+                    rs.getString("DISTANCE") + "\t");
+            }
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    
+}
